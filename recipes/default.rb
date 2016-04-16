db25f19425791d00738ad4be04c91baa0d5dc681
Stephen Pearson <stephen@hpe.com>
2016-04-16 13:31:19 +0100

    Add libvirt-bin

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,6 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+package "libvirt-bin"
 package "cobbler"
 package "bind9"
 package "isc-dhcp-server"
